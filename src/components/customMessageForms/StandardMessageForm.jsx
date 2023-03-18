2ae92f0b9b9c9aa6108abaea80aecb26e93d7f08
Pouish85 <pouish.iphone@gmail.com>
2023-03-18 14:57:58 +0100

    feat: creation of custom submit input

--- src/components/customMessageForms/StandardMessageForm.jsx
+++ src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,13 +1,33 @@
-import { PaperClipIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import { PaperAirplaneIcon, PaperClipIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
 
-const StandardMessageForm = () => {
+const StandardMessageForm = ({ props, activeChat }) => {
   const [message, setMessage] = useState("");
   const [attachment, setAttachment] = useState("");
   const [preview, setPreview] = useState("");
 
   const handleChange = (e) => setMessage(e.target.value);
+  
+  const handleSubmit = async () => {
+    const date = new Date()
+      .toISOString()
+      .replace("T", " ")
+      .replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
+
+    const at = attachment ? [{ blob: attachment, file: attachment.name}]: [];
+    const form = {
+      attachments: at,
+      created: date,
+      sender_username: props.username,
+      text: message,
+      activeChatId: activeChat.id,
+    };
+
+    props.onSubmit(form);
+    setMessage("");
+    setAttachment("");
+  };
 
   return (
     <div className='message-form-container'>
@@ -58,6 +78,15 @@ const StandardMessageForm = () => {
                 </div>
               )}
             </Dropzone>
+
+            <hr className='vertical-line' />
+            <PaperAirplaneIcon 
+              className='message-form-icon-airplane'
+              onClick={() => {
+                setPreview("");
+                handleSubmit();
+              }}
+            />
         </div>
       </div>
     </div>
