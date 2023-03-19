8527491541fa38b0ebb935d3465f4eb6468ec03d
Pouish85 <pouish.iphone@gmail.com>
2023-03-19 15:54:02 +0100

    feat: Back server creation and connexion with openAi GPT 3

--- client/src/components/customMessageForms/MessageFormUi.jsx
+++ client/src/components/customMessageForms/MessageFormUi.jsx
@@ -2,32 +2,14 @@ import { PaperAirplaneIcon, PaperClipIcon, XMarkIcon } from '@heroicons/react/24
 import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
 
-const StandardMessageForm = ({ props, activeChat }) => {
-  const [message, setMessage] = useState("");
-  const [attachment, setAttachment] = useState("");
-  const [preview, setPreview] = useState("");
 
-  const handleChange = (e) => setMessage(e.target.value);
-  
-  const handleSubmit = async () => {
-    const date = new Date()
-      .toISOString()
-      .replace("T", " ")
-      .replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
-
-    const at = attachment ? [{ blob: attachment, file: attachment.name}]: [];
-    const form = {
-      attachments: at,
-      created: date,
-      sender_username: props.username,
-      text: message,
-      activeChatId: activeChat.id,
-    };
-
-    props.onSubmit(form);
-    setMessage("");
-    setAttachment("");
-  };
+const MessageFormUi = ({
+    setAttachment,
+    message,
+    handleChange,
+    handleSubmit
+}) => {
+  const [ preview, setPreview ] = useState("");  
 
   return (
     <div className='message-form-container'>
@@ -91,6 +73,6 @@ const StandardMessageForm = ({ props, activeChat }) => {
       </div>
     </div>
   )
-};
+}
 
-export default StandardMessageForm
+export default MessageFormUi
