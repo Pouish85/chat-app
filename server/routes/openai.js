a8b4b90341f53c9e6b4a5952795665d4e1fe5e7d
Pouish85 <pouish.iphone@gmail.com>
2023-03-19 16:28:16 +0100

    feat: adding channel for code responses

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -37,6 +37,40 @@ router.post('/text', async (req, res) => {
     console.log("error:", error)
     res.status(500).json({ error: error.message })
     }
+});
+
+router.post('/code', async (req, res) => {
+    try {
+        const { text, activeChatId } = req.body;
+
+        const response = await openai.createCompletion({
+            model: "code-davinci-002",
+            prompt: text,
+            temperature: 0.5,
+            max_tokens: 2048,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+
+        await axios.post(
+            `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+            { text: response.data.choices[0].text },
+            {
+                headers: {
+                    "Project-ID": process.env.PROJECT_ID,
+                    "User-Name": process.env.BOT_USER_NAME,
+                    "User-Secret": process.env.BOT_USER_SECRET,
+                }
+            }
+        );
+        console.log("end")
+
+        res.status(200).json({ text: response.data.choices[0].text })
+    } catch (error) {
+        console.log("error:", error.response.data.error);
+        res.status(500).json({ error: error.message });
+    }
 })
 
 export default router;
